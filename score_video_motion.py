9b2c2f7373144c930d64e2b586818f0d56791e3b
sooyeon-go <gojjang385@naver.com>
2026-05-28 21:07:11 +0900

    Initial commit

--- score_video_motion.py
+++ score_video_motion.py
@@ -271,6 +271,9 @@ def main() -> None:
     device = torch.device(args.device)
     raft_model_path = initialize_runtime_paths(args.weight_dir)
 
+     log_progress(
+        f"Start video(s). "
+    )
     video_paths = collect_video_paths(args.video_path, args.video_dir)
     log_progress(
         f"Found {len(video_paths)} video(s). "
