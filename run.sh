8886f7244adf27f019297db3f823f6931d54660b
sooyeon-go <gojjang385@naver.com>
2026-05-28 20:47:41 +0900

    Initial commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+python /data/project-vilab/sy/optical_flow_metric/score_video_motion.py \
+  --video_dir /data/shared-vilab/datasets/OpenVid-1M/video \
+  --weight_dir /data/project-vilab/sy/optical_flow_metric/weight \
+  --save_json /data/project-vilab/sy/test_spotting/optical_flow_metric/output/result.json
